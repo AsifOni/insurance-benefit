86acf9e4bf768ec6e9ae647f342554719c912fbf
AsifOni <asif.oni@astralforminc.com>
2023-08-02 16:14:41 -0400

    Made section editable

--- components/ThreeColumnCard.jsx
+++ components/ThreeColumnCard.jsx
@@ -1,30 +1,24 @@
-import { Card as InsuranceCard } from 'ccg-insurance';
+import { Card as InsuranceCard } from './Card';
 
 export const ThreeColumnCard = (props) => {
   const { title, subText, cardData } = props;
   return (
     <div className="md:container mx-auto pad my-8" data-sb-object-id={props.id}>
-      {title && <h4 className="mb-[0.75rem]">{title}</h4>}
+      {title && (
+        <h4 className="mb-[0.75rem]" data-sb-field-path="title">
+          {title}
+        </h4>
+      )}
 
-      {subText && <p className="mb-[3rem]">{subText}</p>}
+      {subText && (
+        <p className="mb-[3rem]" data-sb-field-path="subText">
+          {subText}
+        </p>
+      )}
 
       <div className="flex flex-row flex-wrap md:flex-nowrap md:space-x-5">
         {cardData?.map((data, idx) => {
-          const { id, topImg, linkText, linkUrl, ...restProps } = data;
-          const { src, alt } = topImg;
-          return (
-            <InsuranceCard
-              key={`card-data-${idx}`}
-              topImg={src}
-              imgSRText={alt}
-              footer={
-                <a className="mt-[1.125rem]" href={linkUrl}>
-                  {linkText}
-                </a>
-              }
-              {...restProps}
-            />
-          );
+          return <InsuranceCard key={`card-data-${idx}`} {...data} />;
         })}
       </div>
     </div>
--- components/TwoColumnSection.jsx
+++ components/TwoColumnSection.jsx
@@ -1,25 +1,13 @@
-import { Card as InsuranceCard } from 'ccg-insurance';
+import { Card as InsuranceCard } from './Card';
 import Image from 'next/image';
 
 export const TwoColumnSection = (props) => {
   const { img: masterImg = {}, cardData = {} } = props;
   const { src: masterSrc, alt: masterAlt } = masterImg;
-  const { id, topImg = {}, linkText, linkUrl, ...restProps } = cardData;
-  const { src, alt } = topImg;
 
   return (
     <div className="md:container mx-auto my-8 flex justify-between" data-sb-object-id={props.id}>
-      <InsuranceCard
-        style={'w-1/3'}
-        topImg={src}
-        imgSRText={alt}
-        footer={
-          <a className="mt-[1.125rem]" href={linkUrl}>
-            {linkText}
-          </a>
-        }
-        {...restProps}
-      />
+      {cardData && <InsuranceCard className={'w-1/2'} {...cardData} />}
       <Image src={masterSrc} alt={masterAlt} width={520} height={412} />
     </div>
   );
