88ec67f377a549e1007905ab4c8a80ed181d3707
AsifOni <asif.oni@astralforminc.com>
2023-08-13 13:03:32 -0400

    preview enabled

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { NinetailedProvider } from '@ninetailed/experience.js-next';
+import { NinetailedPreviewPlugin } from '@ninetailed/experience.js-plugin-preview';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -7,7 +8,26 @@ function MyApp({ Component, pageProps }) {
       /* required */
       clientId={process.env.NEXT_PUBLIC_NINETAILED_CLIENT_ID}
       /* Sets needed plugins */
-      plugins={[]}
+      plugins={[
+        new NinetailedPreviewPlugin({
+          // Required: Experiences from your CMS
+          experiences: pageProps.ninetailed?.preview.allExperiences || [],
+          // Required: Audiences from your CMS
+          audiences: pageProps.ninetailed?.preview.allAudiences || [],
+          // Optional: Callback to handle user forwarding to the experience entry in your CMS
+          onOpenExperienceEditor: (experience) => {
+            if (process.env.CONTENTFUL_SPACE_ID) {
+              window.open(
+                `https://app.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${
+                  process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT || 'master'
+                }/entries/${experience.id}`,
+                '_blank',
+              );
+            }
+          },
+          ui: { opener: { hide: false } },
+        }),
+      ]}
       environment={process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT ?? 'main'}
       /* The maximum loading time until the fallback (Baseline) will be shown */
       requestTimeout={500}
