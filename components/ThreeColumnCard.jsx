f46fb13d0d59b8cadcf71601c76eedf9818dc11f
AsifOni <asif.oni@astralforminc.com>
2023-08-18 21:15:34 -0400

    Used component from library

--- components/ThreeColumnCard.jsx
+++ components/ThreeColumnCard.jsx
@@ -1,26 +1,15 @@
-import { Card as InsuranceCard } from './Card';
+import { ThreeColumnCard as InsuranceThreeColumnCard } from 'ccg-insurance';
 
 export const ThreeColumnCard = (props) => {
-  const { title, subText, cardData } = props;
   return (
     <div className="md:container mx-auto pad my-8" data-sb-object-id={props.id}>
-      {title && (
-        <h4 className="mb-[0.75rem]" data-sb-field-path="title">
-          {title}
-        </h4>
-      )}
-
-      {subText && (
-        <p className="mb-[3rem]" data-sb-field-path="subText">
-          {subText}
-        </p>
-      )}
-
-      <div className="flex flex-row flex-wrap md:flex-nowrap md:space-x-5">
-        {cardData?.map((data, idx) => {
-          return <InsuranceCard key={`card-data-${idx}`} {...data} />;
-        })}
-      </div>
+      <InsuranceThreeColumnCard
+        {...props}
+        sbDataAttr={{
+          title: 'title',
+          subText: 'subText',
+        }}
+      />
     </div>
   );
 };
