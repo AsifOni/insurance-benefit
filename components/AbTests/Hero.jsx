349437772b34fb2e098bdef9a68e326f2f453abd
AsifOni <asif.oni@astralforminc.com>
2023-08-22 17:44:58 -0400

    added style editor options

--- components/AbTests/Hero.jsx
+++ components/AbTests/Hero.jsx
@@ -1,10 +1,16 @@
-export const Hero = ({id, tag, heading, body, href1, href2, ctaLabel1, ctaLabel2  }) => {
+export const Hero = ({id, tag, heading, body, href1, href2, ctaLabel1, ctaLabel2, styles, subtitle, tagColor  }) => {
+
+  // const fontSizeEditor = fontSize?.self?.fontSize;
+  console.log('styles', styles);
+  console.log('subtitle', subtitle);
   return (
     <section className="flex items-center justify-center bg-white" data-sb-object-id={id}>
       <div className="mx-auto max-w-[43rem]">
         <div className="text-center">
-          <p className="text-lg font-medium leading-8 text-indigo-600/95">{tag}</p>
-          <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">
+          <p className="text-lg font-medium leading-8 text-indigo-600/95" style={{color: tagColor}}>{tag}</p>
+          <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black" 
+          // style={{fontSize: `${fontSizeEditor}px`}}
+          >
             {heading}
           </h1>
           <p className="mt-3 text-lg leading-relaxed text-slate-400">
--- stackbit.config.js
+++ stackbit.config.js
@@ -1,5 +1,5 @@
 import { ContentfulContentSource } from '@stackbit/cms-contentful';
-import { getLocalizedFieldForLocale } from '@stackbit/types'
+import { getLocalizedFieldForLocale } from '@stackbit/types';
 
 const config = {
   stackbitVersion: '~0.6.0',
@@ -13,7 +13,50 @@ const config = {
       accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_TOKEN,
     }),
   ],
-  modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
+  modelExtensions: [
+    { name: 'page', type: 'page', urlPath: '/{slug}' },
+    {
+      name: 'abtesthero',
+      fieldGroups: [{ name: 'design', label: 'Colors', icon: 'palette' }],
+      fields: [
+        {
+          name: 'styles',
+          type: 'style',
+          description: 'The styles field is controlled by Stackbit editor',
+          styles: {
+            self: {
+              fontSize: ['14:26:1'],
+            },
+            tag: {
+              fontSize: ['14:26:1']
+            },
+            heading: {
+              fontSize: ['14:26:1']
+            },
+            body: {
+              fontSize: ['14:26:1']
+            }
+        
+          },
+        },
+        {
+          type: 'color',
+          name: 'tagColor',
+          label: 'Tag Color',
+          group: 'design',
+          controlType: 'button-group',
+          options: [
+            { label: 'Narrow', value: 'narrow' },
+            { label: 'Wide', value: 'wide' },
+            { label: 'Full', value: 'full' },
+          ],
+        },
+        { name: 'tagColor', label: 'Tag Color', type: 'color', default: '#ffffff', group: 'design', },
+        { name: 'headingColor', label: 'Heading Color', type: 'color', default: '#ffffff', group: 'design', },
+        { name: 'subheadingColor', label: 'Sub Heading Color', type: 'color', default: '#ffffff', group: 'design', }
+      ],
+    },
+  ],
   // Needed only for importing this repository via https://app.stackbit.com/import?mode=duplicate
   import: {
     type: 'contentful',
@@ -45,11 +88,6 @@ const config = {
       modelName: 'siteConfig',
     },
   ],
-  models: {
-    siteConfig: {
-      type: 'data',
-    },
-  },
 };
 
 export default config;
