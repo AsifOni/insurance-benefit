e71c5ad0c3dbcb45204aa4c05cb9ef0f56d9e4f2
AsifOni <asif.oni@astralforminc.com>
2023-08-25 13:58:53 -0400

    fixed footer build error

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,10 @@
 export const Footer = () => {
   return (
     <>
-      <div className="flex flex-row bg-pattern"></div>
+      <div className="flex flex-row bg-pattern mt-7"></div>
       <footer className="bg-white">
         <div className="md:container md:mx-auto mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-          <div className="md:flex md:justify-between">
+          {/* <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <a href="https://caasco.com/" className="flex items-center">
                 <img src="logo-image.png" className="mr-3" alt="CAA Logo" />
@@ -163,7 +163,7 @@ export const Footer = () => {
               </div>
             </div>
           </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" /> */}
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2023{' '}
