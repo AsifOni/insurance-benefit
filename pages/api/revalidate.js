ca97770208ec668a3528e89aa93f6fe612ec1744
AsifOni <asif.oni@astralforminc.com>
2023-09-14 22:05:32 -0400

    Added revalidate url

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,14 +2,12 @@ export default async function handler(req, res) {
   const requestHeaders = new Headers(req.headers);
   const secret = requestHeaders.get('x-vercel-reval-key');
 
-  console.log('REVALIDATION HITTIN');
-
   if (secret !== process.env.NEXT_CONTENTFUL_REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid secret' });
   }
 
-  try {
-    await res.revalidate(req.query.path);
+  try {    
+    await res.revalidate('/');
     return res.json({
       revalidated: true,
       now: Date.now(),
