d9562b9a087d68f0438454e7c6e169abcb355de0
AsifOni <asif.oni@astralforminc.com>
2023-09-17 16:11:06 -0400

    added ab test component editing

--- components/AbTests/Hero.jsx
+++ components/AbTests/Hero.jsx
@@ -1,3 +1,5 @@
+import { useContentfulInspectorMode } from '@contentful/live-preview/react';
+
 export const Hero = ({
   id,
   tag,
@@ -44,11 +46,13 @@ export const Hero = ({
     'tertiary': 'transform rounded-md border border-white px-5 py-3 font-medium text-white transition-colors hover:bg-slate-50'
   }
 
+  const inspectorProps = useContentfulInspectorMode({ entryId: id })
+
   return (
     <section className="flex items-center justify-center bg-white" data-sb-object-id={id}>
       <div className="flex" style={{ ...applyStyles(styles?.self) }}>
         {img && (
-          <img src={img.src} alt={img.alt} className="w-[561px] h-[541px] object-cover" data-sb-field-path=".img" />
+          <img src={img.src} alt={img.alt} className="w-[561px] h-[541px] object-cover" data-sb-field-path=".img"  {...inspectorProps({ fieldId: 'img' })} />
         )}
         <div className="mx-auto max-w-[43rem] p-8" style={{ backgroundColor, color: bodyColor }}>
           <div className="text-center">
@@ -56,6 +60,7 @@ export const Hero = ({
               className="text-lg font-medium leading-8 text-indigo-600/95"
               style={{ color: tagColor, ...applyStyles(styles?.tag) }}
               data-sb-field-path=".tag"
+              {...inspectorProps({ fieldId: 'tag' })}
             >
               {tag}
             </p>
@@ -63,6 +68,7 @@ export const Hero = ({
               className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black"
               style={{ color: headingColor, ...applyStyles(styles?.heading) }}
               data-sb-field-path=".heading"
+              {...inspectorProps({ fieldId: 'heading' })}
             >
               {heading}
             </h1>
@@ -70,6 +76,7 @@ export const Hero = ({
               className="mt-3 text-lg leading-relaxed text-slate-400"
               style={{ color: bodyColor, ...applyStyles(styles?.body) }}
               data-sb-field-path=".body"
+              {...inspectorProps({ fieldId: 'body' })}
             >
               {body}
             </p>
